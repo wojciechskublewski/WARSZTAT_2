5e7f990277267330c7ee4d00bbf7334ee3b1428d
wojciech.skublewski@wp.pl <wojciech.skublewski@wp.pl>
2018-12-02 15:04:52 +0100

    user

--- src/com/company/db/DatabaseConnection.java
+++ src/com/company/db/DatabaseConnection.java
@@ -1,6 +1,33 @@
 package src.com.company.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseConnection {
 
 
+    private static Connection openConnection =null;
+
+
+    public static Connection getConnection() {
+        try (Connection newConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/szkola_programowania", "root", "coderslab")){
+            return newConnection;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static Connection getEfficientConnection() throws SQLException {
+
+        if (openConnection == null || openConnection.isClosed()) {
+
+            Connection newConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/szkola_programowania", "root", "coderslab");
+            return openConnection;
+        }
+        return null;
+
+    }
 }
